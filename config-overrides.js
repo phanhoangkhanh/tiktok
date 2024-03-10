26a18763ffa19f5a9d34613991b0286705ad431e
unknown <phkhanh1188@gmail.com>
2024-03-10 15:35:00 +0700

    new

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    // nơi cấu hình lai webpack khi cần
+    return config;
+  }
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>TIKTOK</h1>
+        </div>
+    );
+}
+
+export default App;
